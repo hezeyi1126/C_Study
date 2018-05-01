c6ca4f54b571553dcbcbef324138db553f96289c
shou66 <27683139@qq.com>
2018-05-01 23:07:32 +0800

    1
    1

--- dragToRun.bat
+++ dragToRun.bat
@@ -1,10 +1,6 @@
 @echo off
 color 0E
-<<<<<<< HEAD:dragToRun.bat
-echo compiling
-=======
 echo compiling..
->>>>>>> b7e5dc05a3950bec2ea833509bdec4efc818dd50:dragToRun.bat
 gcc %1 -o %~p1%~n1
 echo results:
 echo -------------------------------------------------------------------------
